bccfef490557e216f99511f35d4e9feb38266fd2
Jacob A. Morris <jacob.morris@gmail.com>
2018-10-25 11:28:08 -0700

    updated demo content

--- day-09/js/demo.js
+++ day-09/js/demo.js
@@ -1,1 +1,41 @@
-//js goes here
+// Assign an event listener on an `button` element
+let button = document.querySelector('.clickme');
+button.addEventListener('click', function() {
+    // By default, an `event` argument will be passed to this function
+    let message = "You clicked " + event.offsetX + " and " + event.offsetY;
+    let clickedElem = event.target; //target property of the event
+
+    // Do whatever you're looking to do
+    console.log(message);
+    console.log(clickedElem);
+});
+let test = document.querySelector('[href*="css/none.css"]');
+console.log(test);
+
+function doSomething(item) {
+    document.querySelector(`#${item}`).addEventListener('click', function() {
+        test.setAttribute('href',`css/${item}.css`);
+    })
+}
+
+let listItems = document.querySelectorAll(".styles li");
+for(var i in Object.keys(listItems))  doSomething(listItems[i].id);
+//console.log(listItems);
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowUp'){
+        console.log("Going up!")
+    }
+    //...
+});
+
+let clickCount = 0;  //keep track of the "state" (global)
+document.querySelector('button').addEventListener('click', function() {
+    if(clickCount > 10) {  //decide what to do
+        console.log("I think you've had enough");
+    }
+    else {
+        clickCount++;  //change state (+1)
+        console.log('You clicked me!');
+    }
+});
